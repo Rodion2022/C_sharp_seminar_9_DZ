3863b0df5dcc6cbf221edd948f2bd0c23596fb3b
Rodion Gibadullin <rodion-stalker@mail.ru>
2022-09-08 16:27:04 +0300

    Решение первой задачи - чистовик

---
+++ C_sharp_seminar_9_DZ/1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine()!);
+
+string PrintNumbers(int start, int end)
+{
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+Console.WriteLine(PrintNumbers(numberM, numberN));
